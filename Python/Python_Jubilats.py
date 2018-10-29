b57446d30ce544cfdf30b719bc78f0c7e5a08e44
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-29 13:16:07 +0100

    Update and rename Python_Jubilats to Python_Jubilats.py

---
+++ Python/Python_Jubilats.py
@@ -0,0 +1,12 @@
+1	jubilats=input("Indica si els jubilats paguen o no:")
+2	no_jubilats=int(input("Indica quant paguen els no jubilats:"))
+3	dones_cabell_ros=input("Indica si paguen o no les dones amb el cabell ros:")
+4	dones_cabell_de_diferent_color=int(input("Indica quant paguen les dones de cabell de diferent color:"))
+6	if(edad=="jubilat"):
+7	    print("No paga")
+8	if(edad=="no_jubilat"):
+9	    print("1")
+10	if(dona=="cabell_ros"):
+11	    print("No paga")
+12	if(dona=="cabell_de_diferent_color"):
+13	    print("050")
