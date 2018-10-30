8ef860e2a7e112665da068d35ef1768c36a9117e
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-30 12:23:59 +0100

    Delete Python_Swap_Ejercicio.py

--- Python/Python_Swap_Ejercicio.py
+++
@@ -1,4 +0,0 @@
-mano=input("Indique en que mano tiene el móvil:")
-mano=input("Indique en que mano tiene el bocadillo:")
-if((mano=="derecha") and (mano=="izquierda")):
-    print("Cuidado de que no se te caiga nada")
