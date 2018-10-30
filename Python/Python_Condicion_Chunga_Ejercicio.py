38490d2c6e917b384ee6e8aab9aa954e55e93c76
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-30 12:23:41 +0100

    Delete Python_Condicion_Chunga_Ejercicio.py

--- Python/Python_Condicion_Chunga_Ejercicio.py
+++
@@ -1,4 +0,0 @@
-numero=int(input("Indique el número:"))
-numero=input("Indique si és negativo:")
-if((numero==2,6,8,10) or (-10<=numero<=40) or (numero=="negativo")):
-    print("Ejercicio bien hecho")
