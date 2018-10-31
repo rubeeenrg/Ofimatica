a58c311f17dc5b3496c76055d0690934d2fb2d97
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-31 10:08:34 +0100

    Create Python_Estación.py

---
+++ Exercicis Extra/Python_Estación.py
@@ -0,0 +1,5 @@
+estacion=input("Indique la estacion:")
+temperatura=int(input("Indique la temperatura:"))
+hora=int(input("Indique la hora:"))
+if((estacion=="verano") and (temperatura<=15) and (hora>=0000) and (hora<=2359)):
+    print("Ponerme pantalon largo")
