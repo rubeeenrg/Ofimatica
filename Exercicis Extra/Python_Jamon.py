69bd0345bd99cae9dbc077cee6e6242e9b4e5418
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-31 10:10:30 +0100

    Create Python_Jamon.py

---
+++ Exercicis Extra/Python_Jamon.py
@@ -0,0 +1,5 @@
+jamon=input("Indique si tiene jamon:")
+abducion_jueves=input("Indique si le a abducido un ovni un jueves:")
+estudiar=input("Indique cuanto ha estudiado:")
+if((jamon=="si") or (abducion_jueves=="si") or (estudiar=="mucho")):
+   print("Aprobar MO3")
