228869c6a8e057c0117f9184dc9beb6aa494f854
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-10-31 10:10:41 +0100

    Delete Python_Jamon.py

--- Python/Python_Jamon.py
+++
@@ -1,5 +0,0 @@
-1 jamon=input("Indique si tiene jamon:")
-2 abducion_jueves=input("Indique si le a abducido un ovni un jueves:")
-3 estudiar=input("Indique cuanto ha estudiado:")
-4 if ((jamon=="si") or (abducion_jueves=="si") or (estudiar=="mucho")):
-5    print("Aprobar MO3")
