d60af38de3cb62ba6c288bbe6ae7e83ffd81705b
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-06 15:14:21 +0100

    Update and rename comparador_anyos.py to Python_comparador_anyos.py

---
+++ Exercicis Extra/Python_comparador_anyos.py
@@ -0,0 +1,11 @@
+# codgin: utf -8
+anyo_actual=int(input("Indique el año en el que se encuentra:"))
+anyo_cualquiera=int(input("Indique un año cualquiera:"))
+if(anyo_actual==2015)and(anyo_cualquiera==2015):
+  print("¡2015 y 2015 son el mismo año!")
+else:
+    if(anyo_actual==2015)and(anyo_cualquiera==2020):
+        print("Para llegar al anyo_cualquiera","faltan 5 años")
+    else:
+        if(anyo_actual==2015)and(anyo_cualquiera==1997):
+           print("Desde el año_cualquiera","han pasado 18 años")
