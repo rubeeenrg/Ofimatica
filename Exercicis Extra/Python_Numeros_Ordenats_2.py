04a6d4e7939cd267aed7195adead009e23264ca6
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-08 12:05:29 +0100

    Create Python_Numeros_Ordenats_2.py

---
+++ Exercicis Extra/Python_Numeros_Ordenats_2.py
@@ -0,0 +1,8 @@
+#codding: utf -8
+numero_1=int(input("Indique su número en numero_1:"))
+numero_2=int(input("Indique su número en numero_2:"))
+if(numero_2<numero_1):
+  (numero_3==numero_2)
+  (numero_2==numero_1)
+  (numero_1==numero_3)
+print("numero_1,numero_2")
