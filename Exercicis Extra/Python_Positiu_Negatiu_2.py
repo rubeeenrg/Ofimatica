17f0c1528877b057e5a42af4bafeb2443a235eed
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-08 11:26:29 +0100

    Create Python_Positiu_Negatiu_2.py

---
+++ Exercicis Extra/Python_Positiu_Negatiu_2.py
@@ -0,0 +1,10 @@
+#coding: utf -8
+numero=float(input("Indique un número:"))
+if(numero>0):
+    print("És positivo")
+else:
+    if(numero<0):
+        print("És negativo")
+    else:
+        if(numero==0):
+            print("")
