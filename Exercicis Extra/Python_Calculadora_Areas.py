3f6dbd33b7c6dad9ce78957db9abb159f2f1907a
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-13 13:26:55 +0100

    Update Python_Calculadora_Areas.py

--- Exercicis Extra/Python_Calculadora_Areas.py
+++ Exercicis Extra/Python_Calculadora_Areas.py
@@ -1,13 +1,13 @@
 #coding:utf-8
 opcio=input("Quieres calcular el área de T o C:")
 if(opcio=="T"):
-    base_triangulo=int(input("Indique la base del triangulo:"))
+    base_triangulo=float(input("Indique la base del triangulo:"))
     altura_triangulo=float(input("Indique la altura del triangulo:"))
     calculo=abs(base_triangulo*altura_triangulo/2)
     print("El área del triangulo és de",calculo)
 else:
     pi=3.14159265359
-    radio_circulo=int(input("Indique el radio del circulo:"))
+    radio_circulo=float(input("Indique el radio del circulo:"))
     calculo_2=abs(2*pi*radio_circulo)
     if(opcio=="C"):
         print("El área del circulo és de",calculo_2) 
