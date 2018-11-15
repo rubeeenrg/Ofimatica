5279543fbc67ae0cace65b398ff640163ea2a28f
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-15 10:27:04 +0100

    Update Python_Mostrar_Maxim_1.py

--- Exercicis Extra/Python_Mostrar_Maxim_1.py
+++ Exercicis Extra/Python_Mostrar_Maxim_1.py
@@ -4,8 +4,4 @@ numero_2=float(input("Indique el numero 2:"))
 if(numero_1>numero_2):
     print("És más grande el",numero_1)
 else:
-    if(numero_2>numero_1):
-        print("És más grande el",numero_2)
-    else:
-        if(numero_1==numero_2):
-            print("¡Són iguales!")
+    print("És más grande el",numero_2)
