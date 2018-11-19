14841c3e23703a9d755a195d36197f83c570a7bc
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-19 14:04:53 +0100

    Create Python_Pares.py

---
+++ Exercicis Extra/Python_Pares.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+numero_1=int(input("Indique el número 1:"))
+numero_2=int(input("Indique el número 2:"))
+if(4%2==0):
+    print("Qué bonito número par")
+else:
+    if(5%2==2):
+        print("Qué número más vulgar")
