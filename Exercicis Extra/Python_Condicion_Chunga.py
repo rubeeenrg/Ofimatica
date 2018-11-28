af15bcc28f56802e117b1ac8f88edcb6b6c467a1
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-28 12:58:15 +0100

    Update Python_Condicion_Chunga.py

--- Exercicis Extra/Python_Condicion_Chunga.py
+++ Exercicis Extra/Python_Condicion_Chunga.py
@@ -1,1 +1,14 @@
-
+numero=int(input("Introduzca aquí el número que usted quiera:"))
+if(numero%2==0)
+    print("Este número és un número par")
+else:
+    print("Este número és un número impar")
+    if(numero>=-10)and(numero<=40):
+        print("Este número está entre -10 y 40")
+        else:
+          print("Este número no está entre -10 y 40")
+             if(numero<0)
+                 print("Este número és negativo")
+             else:
+                 print("Este número no és negativo")
+    
