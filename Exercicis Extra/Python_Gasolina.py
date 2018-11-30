53c9262e08cc0f9fca6f3d21c31b8d7fee25a988
ruben20022002 <44255144+ruben20022002@users.noreply.github.com>
2018-11-30 11:24:10 +0100

    Update Python_Gasolina.py

--- Exercicis Extra/Python_Gasolina.py
+++ Exercicis Extra/Python_Gasolina.py
@@ -1,26 +1,48 @@
 #coding:utf-8
-gasolina=input("Indique que tipo de gasolina quiere:")
-super=input("Indique si quiere gasolina normal o turbo:")
-sin_plomo=input("Indique si quiere gasolina normal o con aditivios (sabor naranja):")
-diesel=input("Indique si quiere gasolina normal o fast&furius:")
-if(gasolina=="super") and (super=="normal"):
-    print("1.50€")
+print("Tipo de gasolina:")
+print("Super:")
+print("  1.Normal: 1.5€")
+print("  2.Turbo: 1.55€")
+print("Sin Plomo")
+print("  3.Normal: 1.6€")
+print("  4.Con Aditivos(sabor naranja): 1.65€")
+print("Diesel:")
+print("  5.Normal: 1.7€")
+print("  6.Fast&Furious: 1.75€")
+super_normal=1
+super_turbo=2
+sin_normal=3
+sin_aditivos=4
+diesel_normal=5
+diesel_fastfurious=6
+precio_1=1.5
+precio_2=1.55
+precio_3=1.6
+precio_4=1.65
+precio_5=1.7
+precio_6=1.75
+eleccion=int(input("Número de la gasolina deseada: "))
+litros=int(input("¿Cuatos litros quiere usted? "))
+if(eleccion==1):
+    resultado=(litros*precio_1)
+    print("Importe:", resultado,"€")
 else:
-    if(gasolina=="super") and (super=="turbo"):
-        print("1.55€")
+    if(eleccion==2):
+        resultado=(litros*precio_2)
+        print("Importe:", resultado,"€")
     else:
-        if(gasolina=="sin_plomo") and (sin_plomo=="normal"):
-            print("1.60€")
+        if(eleccion==3):
+            resultado=(litros*precio_3)
+            print("Importe:", resultado,"€")
         else:
-             if(gasolina=="sin_plomo") and (sin_plomo=="con aditivios (sabor naranja)"):
-                    print("1.65€")
-             else:
-                  if(gasolina=="diesel") and (diesel=="normal"):
-                     print("1.70€")
-                  else:
-                       if(gasolina=="diesel") and (diesel=="fast&furius"):
-                          print("1.75€")
-                       else:
-litros=float(input("Indique cuantos litros quiere:")
-importe=abs(gasolina*litros)
-print("Su importe és de", importe)
+            if(eleccion==4):
+                resultado=(litros*precio_4)
+                print("Importe:", resultado,"€")
+            else:
+                if(eleccion==5):
+                    resultado=(litros*precio_5)
+                    print("Importe:", resultado,"€")
+                else:
+                    if(eleccion==6):
+                        resultado=(litros*precio_6)
+                        print("Importe:", resultado,"€")
